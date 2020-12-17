22a2d00456e380784f06bcd6aff9bd56ada2894d
J3mCode <jmarquez1047@outlook.com>
2020-12-17 07:23:28 +0000

    jasmine was not working, moved folders and tried testing. fixed error

--- jasmine-testing/spec/calcSpec.js
+++ jasmine-testing/spec/calcSpec.js
@@ -1,5 +1,5 @@
 describe("calculator", function(){
-    describe("addition tests", function(){
+    describe("Addition tests", function(){
         it("should return 42", function(){
             expect(addition(20,22)).toBe(42);
         });
